7ba99d543eb0f535ac6aed0daa08e09535118a35
okma01 <m.oktamaulana6@gmail.com>
2024-03-28 18:23:37 +0700

    Memanggil menthod dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa =====================");
+  System.out.println("NPM : 2210020039");
+  System.out.println("Nama : Muhammad Okta Maulana");
+  System.out.println("=======================================");
+ }
+}
